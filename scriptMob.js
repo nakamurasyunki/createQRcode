694d8183d22e3fa837a81767ef29f039dc069ca9
nakamurasyunki <shiyunki_nakamura@mail.toyota.co.jp>
2023-09-18 16:11:21 +0900

    push

--- scriptMob.js
+++ scriptMob.js
@@ -21,7 +21,8 @@ document.getElementById('getqrInfo-mob').addEventListener('click', () => {
     document.getElementById('qrDownload-mob').style.visibility = 'visible';
   })();
   // png出力用コード
-  //var cvs = document.getElementById("qr");
-  //var png = cvs.toDataURL();
-  //document.getElementById("newImg").src = png;
+  var cvs = document.getElementById("qr");
+  var png = cvs.toDataURL();
+  document.getElementById("newImg").src = png;
+  document.getElementById('qrOutput-mob').style.display = 'none';
 });
